0c4e8579c217d363b523cac07e798176312f328e
Yashashree <yashashreesuresh@gmail.com>
2021-06-04 21:55:15 +0530

    Adding selenium basics

---
+++ selenium_basics/basics.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.chrome.options import Options
+
+chrome_options = Options()
+chrome_options.add_argument("--headless")
+
+driver = webdriver.Chrome(executable_path="./chromedriver", options=chrome_options)
+driver.get("https://duckduckgo.com/")
+
+search_input = driver.find_element_by_id("search_form_input_homepage")
+search_input.send_keys("Hello World!")
+
+# Search by clicking the search button
+# search_btn = driver.find_element_by_id("search_button_homepage")
+# search_btn.click()
+
+# Search using pressing ENTER key
+search_input.send_keys(Keys.ENTER)
+
+print(driver.page_source)
+
+driver.close()
